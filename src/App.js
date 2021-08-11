9c6dff1a26aad15358844b58c745121457723544
PRETHIVEE <kgprethivee@gmail.com>
2021-08-11 17:58:43 +0530

    basic login form created
    just a single login form created .
    installed Joi for form validation.
    next  modifying forms to reuseable components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Customers from "./components/customers";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <main className="container">
         <Switch>
           {/* path "/movies/:id" should Start from specific to generic */}
+          <Route path="/login" component={LoginForm}></Route>
           <Route path="/movies/:id" component={MovieForm}></Route>
           <Route path="/movies" component={Movies}></Route>
           <Route path="/customers" component={Customers}></Route>
